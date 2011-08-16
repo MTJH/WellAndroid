a48f6dd6bd8c22204d7301cc3c71438ea0e3dceb
gexueyuan <gexueyuan@gmail.com>
2011-08-16 17:27:45 +0800

    progress dot

--- gen/gxy/well/R.java
+++ gen/gxy/well/R.java
@@ -13,16 +13,26 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int init=0x7f020001;
-        public static final int normaltest=0x7f020002;
-        public static final int record=0x7f020003;
-        public static final int screentest=0x7f020004;
-        public static final int setting=0x7f020005;
+        public static final int initdot1=0x7f020002;
+        public static final int initdot2=0x7f020003;
+        public static final int initdot3=0x7f020004;
+        public static final int normaltest=0x7f020005;
+        public static final int record=0x7f020006;
+        public static final int screentest=0x7f020007;
+        public static final int setting=0x7f020008;
     }
     public static final class id {
-        public static final int NormalTest=0x7f050004;
-        public static final int QuickTest=0x7f050003;
-        public static final int ScreenTest=0x7f050002;
+        public static final int NormalTest=0x7f05000b;
+        public static final int QuickTest=0x7f05000a;
+        public static final int ScreenTest=0x7f050009;
+        public static final int dot=0x7f050002;
         public static final int init=0x7f050001;
+        public static final int initdot1=0x7f050003;
+        public static final int initdot2=0x7f050004;
+        public static final int initdot3=0x7f050005;
+        public static final int initdot4=0x7f050006;
+        public static final int initdot5=0x7f050007;
+        public static final int initdot6=0x7f050008;
         public static final int left=0x7f050000;
     }
     public static final class layout {
